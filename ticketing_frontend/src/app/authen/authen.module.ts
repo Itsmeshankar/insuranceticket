8ef05b9e99d46fadf022d3f36d18e9200e9540a2
unknown <shankar.paudel1994@gmail.com>
2024-02-14 21:37:05 +0545

    2024-02-04

--- ticketing_frontend/src/app/authen/authen.module.ts
+++ ticketing_frontend/src/app/authen/authen.module.ts
@@ -6,11 +6,13 @@ import { LoginComponent } from './views/login.component';
 import { ComponentsModule } from '../components/views/components.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RegisterComponent } from './views/register.component';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
